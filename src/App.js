a6ebc2e2bfbbea575db683c8e38797dd30c35542
caue peruque <caue19052003@gmail.com>
2023-05-22 17:48:26 -0300

    iniciando req 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Login from './components/Login';
+import Login from './pages/Login';
 import './App.css';
 
 export default function App() {
