6385020b1c599e60709c669994f1b4fe008bdc3d
Roberto Mello <robertomelloalmeida@outlook.com>
2023-05-23 14:09:22 -0300

    req 4 testando mock

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,103 +1,102 @@
 import React from 'react';
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux'
-import Login, { handleChange } from '../pages/Login';
-import App from '../App'
-
+import { act } from 'react-dom/test-utils';
+import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+import Login from '../pages/Login';
+import App from '../App';
 
 describe('Teste Login', () => {
   it('Verifica se o campo nome está renderizado na tela', () => {
-    renderWithRouterAndRedux(<Login />)
+    renderWithRouterAndRedux(<Login />);
 
-    const inputName = screen.getByTestId('input-player-name')
+    const inputName = screen.getByTestId('input-player-name');
 
     expect(inputName).toBeInTheDocument();
-  })
+  });
 
   it('verifica se o campo email está renderizado na tela', () => {
-    renderWithRouterAndRedux(<Login />)
+    renderWithRouterAndRedux(<Login />);
 
-    const inputEmail = screen.getByTestId('input-gravatar-email')
+    const inputEmail = screen.getByTestId('input-gravatar-email');
 
     expect(inputEmail).toBeInTheDocument();
-  })
+  });
 
   it('verifica se o botão Play está renderizado na tela', () => {
-
-    renderWithRouterAndRedux(<Login />)
+    renderWithRouterAndRedux(<Login />);
 
     screen.getByRole('button', {
       name: /play/i
-    })
-
-  })
+    });
+  });
 
   it('verifica se o botão Play está desabilitado ao renderizar a página', () => {
-
-    renderWithRouterAndRedux(<Login />)
+    renderWithRouterAndRedux(<Login />);
     const btnPlay = screen.getByRole('button', {
       name: /play/i
-    })
-    expect(btnPlay.disabled).toBe(true)
-  })
-
+    });
+    expect(btnPlay.disabled).toBe(true);
+  });
 
   it('verifica se o botão settings está renderizado na tela', () => {
+    renderWithRouterAndRedux(<Login />);
 
-    renderWithRouterAndRedux(<Login />)
-
-    screen.getByTestId('btn-settings')
-  })
+    screen.getByTestId('btn-settings');
+  });
 
   it('verifica a função handleChange', () => {
+    renderWithRouterAndRedux(<Login />);
 
-    renderWithRouterAndRedux(<Login />)
-
-    const inputName = screen.getByTestId('input-player-name')
-    const inputEmail = screen.getByTestId('input-gravatar-email')
+    const inputName = screen.getByTestId('input-player-name');
+    const inputEmail = screen.getByTestId('input-gravatar-email');
     const btn = screen.getByRole('button', {
       name: /play/i
-    })
+    });
 
-    userEvent.type(inputName, 'abc')
-    userEvent.type(inputEmail, 'abc@abc.com')
+    userEvent.type(inputName, 'abc');
+    userEvent.type(inputEmail, 'abc@abc.com');
 
     expect(btn).toBeEnabled();
-  })
+  });
 
   it('verifica a função handleClick', async () => {
+    const { history } = renderWithRouterAndRedux(<App />);
 
-    const { history } = renderWithRouterAndRedux(<App />)
+    const mockResultadoAPI = 'abc';
 
-    const inputName = screen.getByTestId('input-player-name')
-    const inputEmail = screen.getByTestId('input-gravatar-email')
-    const btn = screen.getByRole('button', {
-      name: /play/i
-    })
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValueOnce({
+    json: jest.fn().mockResolvedValue(mockResultadoAPI),
+  });
 
-    userEvent.type(inputName, 'abc')
-    userEvent.type(inputEmail, 'abc@abc.com')
+    const inputName = screen.getByTestId('input-player-name');
+    const inputEmail = screen.getByTestId('input-gravatar-email');
+    const btn = screen.getByTestId('btn-play');
+
+    userEvent.type(inputName, 'abc');
+    userEvent.type(inputEmail, 'abc@abc.com');
 
     expect(btn).toBeEnabled();
 
-    userEvent.click(btn)
+    userEvent.click(btn);
     await waitFor(() => {
       const { pathname } = history.location;
       expect(pathname).toBe('/play');
     });
-  })
+  });
 
   it('verifica a função clickBtn', async () => {
+    const { history } = renderWithRouterAndRedux(<App />);
 
-    const { history } = renderWithRouterAndRedux(<App />)
+    const btn = screen.getByTestId('btn-settings');
 
-    const btn = screen.getByTestId('btn-settings')
-
-    userEvent.click(btn)
-    await waitFor(() => {
-      const { pathname } = history.location;
-      expect(pathname).toBe('/settings');
+    await act(async () => {
+      userEvent.click(btn);
+      await waitFor(() => {
+        const { pathname } = history.location;
+        expect(pathname).toBe('/settings');
+      });
     });
-  })
-})
+  });
+});
