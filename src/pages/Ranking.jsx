586289ee9c6a3775a5761655789f9f987367f5ec
delso-ferreira <delsofelipe@gmail.com>
2023-05-24 17:42:18 -0300

    fix lint

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
   handleClick = () => {
-    const {history} = this.props
-    history.push('/')
-  }
+    const { history } = this.props;
+    history.push('/');
+  };
 
   render() {
     return (
-    <div data-testid="ranking-title">
-      Ranking
-      <button 
-      onClick={this.handleClick}
-      data-testid="btn-go-home">
-      Go Home
-      </button>
-    </div>
-      );
+      <div data-testid="ranking-title">
+        Ranking
+        <button
+          onClick={ this.handleClick }
+          data-testid="btn-go-home"
+        >
+          Go Home
+        </button>
+      </div>
+    );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
 
 export default connect()(Ranking);
