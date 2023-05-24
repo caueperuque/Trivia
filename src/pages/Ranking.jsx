46a764430beff6de0da5b9099b8b358b0a63a133
delso-ferreira <delsofelipe@gmail.com>
2023-05-24 17:38:20 -0300

    requisito 18 ok

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Ranking extends Component {
+  handleClick = () => {
+    const {history} = this.props
+    history.push('/')
+  }
+
   render() {
     return (
-      <div data-testid="ranking-title">Ranking</div>
-    );
+    <div data-testid="ranking-title">
+      Ranking
+      <button 
+      onClick={this.handleClick}
+      data-testid="btn-go-home">
+      Go Home
+      </button>
+    </div>
+      );
   }
 }
 
